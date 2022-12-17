12ee88bb2dd71c6963b502b685c85c95e8817d15
travisedwards <xctrackrunner2222@yahoo.com>
2022-12-17 12:49:38 -0800

    add headers and footers, dec 17

--- pdfmain.py
+++ pdfmain.py
@@ -2,18 +2,31 @@ from fpdf import FPDF
 import pandas
 
 pdf = FPDF(orientation="P", unit="mm", format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 
 df = pandas.read_csv("topics.csv")
 
 for index, row in df.iterrows():
+
+    # Set header
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border="B")
     num_pages = int(row['Pages']) - 1
-    while num_pages > 0:
+
+    # Set footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="B", size=8)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for i in range(num_pages):
         pdf.add_page()
-        num_pages = num_pages-1
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="B", size=8)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
